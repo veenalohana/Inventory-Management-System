3ab01a57be3b2e41f780e2766589453fb73142b4
veenalohana <lohanaveena38@gmail.com>
2026-09-18 19:23:10 +0500

    Connect db.php to Clever Cloud MySQL

--- inventory-api/db.php
+++ inventory-api/db.php
@@ -1,16 +1,17 @@
 <?php
 // db.php
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "inventory_db";
+$host = "bolwt9qitpbm8xpw6jty-mysql.services.clever-cloud.com";
+$user = "ufvwbkngshyvdiya";
+$password = "AYzU147UV4kvu6EzSIAR";
+$dbname = "bolwt9qitpbm8xpw6jty";
+$port = 3306;
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname, $port);
 
 if ($conn->connect_error) {
     die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
